688ac0fc21954f8136196cd95d2fa5f06498e62f
Jonathan Halpern <jonathanhalpern@jonathans-mbp.home>
2017-12-24 13:06:25 +0000

    added completed booleans

--- src/pages/blog/2017-01-04-a-beginners-guide-to-brewing-with-chemex.md
+++ src/pages/blog/2017-01-04-a-beginners-guide-to-brewing-with-chemex.md
@@ -7,6 +7,8 @@ description: >-
   Brewing with a Chemex probably seems like a complicated, time-consuming
   ordeal, but once you get used to the process, it becomes a soothing ritual
   that's worth the effort every time.
+completed: false
+
 ---
 ![chemex](/img/nat family.jpg)
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -28,29 +28,31 @@ export default class IndexPage extends React.Component {
         />
         <div className="container">
           <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+            <h1 className="has-text-weight-bold is-size-2">Challenges</h1>
           </div>
-          {posts.filter(post => post.node.frontmatter.templateKey === 'blog-post').map(({ node: post }) => {
-            return (
-              <div className="content" style={{ border: '1px solid #eaecee', padding: '2em 4em' }} key={post.id}>
-                <p>
-                  <Link className="has-text-primary" to={post.frontmatter.path}>
-                    {post.frontmatter.title}
-                  </Link>
-                  <span> &bull; </span>
-                  <small>{post.frontmatter.date}</small>
-                </p>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button is-small" to={post.frontmatter.path}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </div>
-            );
-          })}
+          <div className="post-list">
+            {posts.filter(post => post.node.frontmatter.templateKey === 'blog-post').map(({ node: post }) => {
+              return (
+                <div className="content post-container" style={{ border: '1px solid #eaecee', padding: '2em 4em' }} key={post.id}>
+                  <p>
+                    <Link className="has-text-primary" to={post.frontmatter.path}>
+                      {post.frontmatter.title}
+                    </Link>
+                    <span> &bull; </span>
+                    <small>{post.frontmatter.date}</small>
+                  </p>
+                  <p>
+                    {post.excerpt}
+                    <br />
+                    <br />
+                    <Link className="button is-small" to={post.frontmatter.path}>
+                      Keep Reading →
+                    </Link>
+                  </p>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </section>
     );
