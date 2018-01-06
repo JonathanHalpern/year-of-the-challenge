9f092a8dd74240b2334ae2e60701de24e7d8eba3
Jonathan <jonnyhalpern@gmail.com>
2018-01-06 17:22:10 +0700

    Added more emojis and adjust cms select

--- src/services/emojiPicker.js
+++ src/services/emojiPicker.js
@@ -1,11 +1,17 @@
 import happyEmoji from '../../static/img/Functional/happy_emoji.png';
 import tentativeEmoji from '../../static/img/Functional/tentative_emoji.png';
-import anguishEmoji from '../../static/img/Functional/anguish_emoji.png';
+import anguishedEmoji from '../../static/img/Functional/anguished_emoji.png';
+import excitedEmoji from '../../static/img/Functional/excited_emoji.png';
+import confusedEmoji from '../../static/img/Functional/confused_emoji.png';
+import embarrassedEmoji from '../../static/img/Functional/embarrassed_emoji.png';
 
 const EMOJIS = [
+  excitedEmoji,
   happyEmoji,
+  confusedEmoji,
   tentativeEmoji,
-  anguishEmoji
+  embarrassedEmoji,
+  anguishedEmoji
 ]
 
 const getEmojisByChallengeNumber = (challengeNumber) => {
