1270a7726b312b6b6173d5f944ebf5595ae8ada9
Jonathan <jonnyhalpern@gmail.com>
2018-01-14 13:58:45 +0700

    performance 1

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -10,7 +10,6 @@ export const BlogPostTemplate = ({ content, contentComponent, description, title
       <div className="columns">
         <div className="column is-10 is-offset-1">
           <h1 className="title is-size-2 has-text-weight-bold is-bold-light">{title}</h1>
-          <p>{description}</p>
           <PostContent content={content} />
         </div>
       </div>
