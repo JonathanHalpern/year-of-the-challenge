5633df258cae8b82da623b08f1c8577fdb8451a7
Jonathan <jonnyhalpern@gmail.com>
2018-04-26 12:40:01 +0800

    checking content type

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,3 +1,10 @@
 import React from 'react';
 
-export default ({ content }) => <div>{content}</div>;
+export default ({ content }) => {
+  if (typeof (content) === 'string') {
+    return (<div
+      dangerouslySetInnerHTML={{ __html: content }}
+    />);
+  }
+  return <div>{content}</div>;
+};
