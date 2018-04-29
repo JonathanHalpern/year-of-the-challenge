1107fd2996cb417cc7b0af715c43eeb053ce1b42
Jonathan <jonnyhalpern@gmail.com>
2018-04-29 16:52:52 +0800

    mobile comment styling

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -3,18 +3,16 @@ import CommentItem from './CommentItem';
 
 const CommentList = props => (
   <div>
-    <ul>
-      {
-        props.comments.map(comment => (
-          <CommentItem
-            name={comment.node.frontmatter.name}
-            message={comment.node.excerpt}
-            date={comment.node.frontmatter.date}
-            key={comment.node.id}
-          />
-        ))
-      }
-    </ul>
+    {
+      props.comments.map(comment => (
+        <CommentItem
+          name={comment.node.frontmatter.name}
+          message={comment.node.excerpt}
+          date={comment.node.frontmatter.date}
+          key={comment.node.id}
+        />
+      ))
+    }
   </div>
 );
 
