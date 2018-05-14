6c0ec2418a2833094cc4bc5056ede1b15d832cc5
Jonathan <jonnyhalpern@gmail.com>
2018-05-14 16:01:26 +0800

    hid most posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,70 +1,69 @@
-import React from 'react';
-import Helmet from 'react-helmet';
-import styled from 'styled-components';
-import Divider from 'material-ui/Divider';
-import CommentForm from '../components/CommentForm';
-import CommentList from '../components/CommentList';
-import Content, { HTMLContent } from '../components/Content';
+import React from 'react'
+import Helmet from 'react-helmet'
+import styled from 'styled-components'
+import Divider from 'material-ui/Divider'
+import CommentForm from '../components/CommentForm'
+import CommentList from '../components/CommentList'
+import Content, { HTMLContent } from '../components/Content'
 
 const StyledSection = styled.section`
-  p {
-    display: flex;
-    img {
-      max-width: 400px;
-      height: 100%;
-      margin: 0 auto;
-    }
-  }
   .iframeContainer {
-    position:relative;
-    padding-bottom:56.25%;
-    padding-top:30px;
-    height:0;
-    overflow:hidden;
-    iframe, object, embed {
-      position:absolute;
-      top:0;
-      left:0;
-      width:100%;
-      height:100%;
+    position: relative;
+    padding-bottom: 56.25%;
+    padding-top: 30px;
+    height: 0;
+    overflow: hidden;
+    iframe,
+    object,
+    embed {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
     }
   }
-`;
+`
 
-export const BlogPostTemplate = ({ content, title, path, helmet, comments, isCms }) => (
+export const BlogPostTemplate = ({
+  content,
+  title,
+  path,
+  helmet,
+  comments,
+  isCms,
+}) => (
   <StyledSection>
-    { helmet }
+    {helmet}
     <h1>{title}</h1>
-    {
-      isCms ?
-        <Content content={content} /> :
-        <HTMLContent content={content} />
-    }
+    {isCms ? <Content content={content} /> : <HTMLContent content={content} />}
     <Divider />
-    {
-      !isCms && <div>
-        { comments && <CommentList comments={comments.edges} /> }
+    {!isCms && (
+      <div>
+        {comments && <CommentList comments={comments.edges} />}
         <CommentForm postName={path} />
       </div>
-    }
+    )}
   </StyledSection>
-);
+)
 
 export default ({ data: { currentChallengeMarkdown, commentsMarkdown } }) => (
   <BlogPostTemplate
     content={currentChallengeMarkdown.html}
     description={currentChallengeMarkdown.frontmatter.description}
-    helmet={<Helmet title={`Blog | ${currentChallengeMarkdown.frontmatter.title}`} />}
+    helmet={
+      <Helmet title={`Blog | ${currentChallengeMarkdown.frontmatter.title}`} />
+    }
     title={currentChallengeMarkdown.frontmatter.title}
     path={currentChallengeMarkdown.frontmatter.path}
     isCompleted={currentChallengeMarkdown.frontmatter.isCompleted}
     comments={commentsMarkdown}
   />
-);
+)
 
 export const pageQuery = graphql`
   query BlogPostByPath($path: String!) {
     ...CurrentChallengeFragment
     ...CommentsMarkdownFragment
   }
-`;
+`
