ae816a58f24cd42668f38ca9096936c2f6dc119f
Jonathan <jonnyhalpern@gmail.com>
2018-05-20 09:52:11 +0800

    netlify image widget

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,12 @@ module.exports = {
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/pages`,
